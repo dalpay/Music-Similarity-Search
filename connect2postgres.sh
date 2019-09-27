9cb8b9d1b04321a142d6e6188c09aaa74d61949a
Deniz Alpay <deniza@uw.edu>
2019-09-27 09:40:35 -0700

    Reconfigured postgres

---
+++ connect2postgres.sh
@@ -0,0 +1,1 @@
+psql --host=$POSTGRES_HN --username=$POSTGRES_UN --password --port=5432 --dbname=postgres
--- postgres.py
+++ postgres.py
@@ -1,15 +1,15 @@
-from pyspark.sql import DataFrameWriter
 import os
+from pyspark.sql import DataFrameWriter
 
 
 class PostgresConnector(object):
 
     def __init__(self):
-        self.database_name = 'postgres-db'
-        self.hostname = os.environ['HOSTNAME']
+        self.database_name = 'postgres'
+        self.hostname = os.environ['POSTGRES_HN']
         self.url_connect = "jdbc:postgresql://{hostname}:5432/{db}".format(hostname=self.hostname, db=self.database_name)
-        self.properties = {"user": os.environ['USERNAME'],
-                        "password": os.environ['PASSWORD'],
+        self.properties = {"user": os.environ['POSTGRES_UN'],
+                        "password": os.environ['POSTGRES_PW'],
                         "driver": "org.postgresql.Driver"
         }
 
