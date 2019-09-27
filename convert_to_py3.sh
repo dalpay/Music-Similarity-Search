8f4e9d00c6c6bcd89b3502aa4afff8a22c3881ba
Deniz Alpay <deniza@uw.edu>
2019-09-26 21:08:04 -0700

    Specified full path

--- convert_to_py3.sh
+++ convert_to_py3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+dir="/home/ubuntu/MSongsDB/PythonSrc"
 mkdir /home/ubuntu/MSongsDB/Python3Src
-for file in $(find .)
+for file in "$dir/*.py"
 do
     echo $file
     pt2to3 $file > temp.txt
     new_file=$(echo $file | cut -d '/' -f 2)
     mv temp.txt /home/ubuntu/MSongsDB/Python3Src/$new_file
 done
-
