8906b1ce15b2d27a841397a4e84820db4d4b3393
Deniz Alpay <deniza@uw.edu>
2019-10-02 11:35:08 -0700

    Added postgres DB setup

---
+++ scripts/setup_postgres_db.sql
@@ -0,0 +1,16 @@
+-- PostgreSQL database tables creation
+
+CREATE TABLE song_info (
+    song_id     BIGINT PRIMARY KEY,
+    source_id   VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL, 
+    artist      VARCHAR(255) NOT NULL,
+    year        INT,
+    source      VARCHAR(255) CHECK (source IN ('msd', 'spotify'))
+);
+
+CREATE TABLE song_vectors (
+    song_id     BIGINT PRIMARY KEY,
+    vector      FLOAT(32)[] NOT NULL,
+    method      VARCHAR(255) CHECK (method in ('gauss', 'gmm', 'pca', 'cnn'))
+);
