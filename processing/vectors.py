1624802715fc9c35b81d209e0f29b33a59eb61f8
Deniz Alpay <deniza@uw.edu>
2019-10-04 16:02:37 -0700

    Fixed typo and made input case independent

--- processing/vectors.py
+++ processing/vectors.py
@@ -5,8 +5,8 @@ def vector_processor(method='gauss'):
     '''
     Returns the function to compute the embedding vector for a given method.
     Since Spark UDF does not accept keyword arguments, the method keyword 
-    argument is seperated from function that computes the vector by placing it 
-    within this function.
+    argument is seperated from the function that computes the vector by placing 
+    it within this function.
     '''
 
     def compute_vector(*features):
@@ -15,11 +15,11 @@ def vector_processor(method='gauss'):
 
         for feature in features:
 
-            if (method == 'gauss'):
+            if (method.lower() == 'gauss'):
                 vec = gauss_vector(feature)
-            elif (method == 'gmm'):
+            elif (method.lower() == 'gmm'):
                 vec = gmm_vector(feature)
-            elif (method == 'pca'):
+            elif (method.lower() == 'pca'):
                 vec = pca_vector(feature)
 
             if (vec is not None):
